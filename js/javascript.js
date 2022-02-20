e0d1ffcd1e95d98de7c72c96aed59e73a1c9d252
Pikrikun <97285956+Pikrikun@users.noreply.github.com>
2022-02-20 03:31:10 -0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+const slider = document.querySelectorAll(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+});
+
+const sidenav = document.querySelectorAll(".sidenav");
+M.Sidenav.init(sidenav);
+
+const parallax = document.querySelectorAll(".parallax");
+M.Parallax.init(parallax);
+
+const materialbox = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialbox);
